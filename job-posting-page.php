f699a29e074ec447b4b8f2dbfeafd3d4fb274630
Nguyen Sanh Toan <stoannguyen2205@gmail.com>
2016-11-16 14:20:42 +0700

    create job-posting-page

---
+++ job-posting-page.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title>Sparking Jobs</title>
+
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+	<script type="text/javascript" src="assets/js/jquery-1.12.3.min.js"></script>	
+
+</head>
+<body>
+	<div id="header">
+		<div class="container-fluid">
+			<div class="row">
+				<div id="header_top">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-12">
+								<i class="fa fa-bars" aria-hidden="true"></i>
+								<div class="header_logo">
+									<a href="#"><img src="assets/images/header_logo.png" alt="Sparking Jobs"></a>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="body_content">
+		<div class="container-fluid">
+			<div class="row">
+				<div id="body_content_top">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-7 body_job_posting">
+								<form action="">
+									<div class="block_form_one">
+										<label class="number_form">1</label>
+										<div class="form_group_post">
+											<label for="" class="label_form">Describe your work please?</label>
+											<select class="form-control" id="select_op_form">
+												<option>Website IT & Software</option>
+												<option>Mobile</option>
+												<option>Writing</option>
+												<option>Design</option>
+												<option>Data Entry</option>
+												<option>Product Sourcing & Manufacturing</option>
+												<option>Sales & marketing</option>
+												<option>Business, Accounting & Legal</option>
+												<option>Local Jobs & Services</option>
+												<option>Customized (Select skills manually)</option>
+												<option>Select a category of work (optional)</option>
+											</select>
+										</div>
+										<div class="clr"></div>
+									</div>
+									<div class="block_form_two">
+										<label class="number_form">2</label>
+										<div class="form_group_post">
+											<label for="" class="label_form">Describe your work project?</label>
+											<label for="" class="project_name">PROJECT NAME</label>
+											<input type="text" placeholder="e.g. Build a website">
+											<label for="" class="label_three">Does your project require a local freelancer?</label>
+										</div>
+										<div class="clr"></div>
+									</div>
+									<div class="block_form_three">
+										<label class="number_form">3</label>
+										<div class="form_group_post">
+											<label for="" class="label_form">What is more in your project.</label>
+											<label for="" class="project_name">WHAT SKILLS ARE REQUIRED</label>
+											<input type="text" placeholder="What skill are required">
+											<label for="" class="describe_form project_name">DESCRIBE YOUR PROJECT</label>
+											<textarea class="textarea_form" rows="5" placeholder="Describe your project here..."></textarea>
+											<button><i class="fa fa-plus" aria-hidden="true"></i> Upload Files</button>
+											<label for="" class="upload_form">Drag & drop any images or documents that might be helpful in explaining your project brief here</label>
+										</div>
+										<div class="clr"></div>
+									</div>
+									<div class="block_form_four">
+										<label class="number_form">4</label>
+										<div class="form_group_post">
+											<label for="" class="label_form">What budget do you have in mind?</label>
+
+											<div class="full_radio_button">
+												<input type="radio" name="filter" id="all" value="SET FIXED PRICE" checked="checked">
+												<label for="all" class="label_radio">SET FIXED PRICE</label>
+												<input type="radio" name="filter" id="sale" value="SET AN HOURLY RATE">
+												<label for="sale" class="label_radio">SET AN HOURLY RATE</label>
+												<div class="clr"></div>
+											</div>
+											<div class="select_op_full">
+												<select class="select_op_inr">
+													<option>Website IT & Software</option>
+													<option>Mobile</option>
+													<option>Writing</option>
+													<option>Design</option>
+													<option>Data Entry</option>
+													<option>Product Sourcing & Manufacturing</option>
+													<option>Sales & marketing</option>
+													<option>Business, Accounting & Legal</option>
+													<option>Local Jobs & Services</option>
+													<option>Customized (Select skills manually)</option>
+													<option>Select a category of work (optional)</option>
+												</select>
+												<select class="select_op_price">
+													<option>Website IT & Software</option>
+													<option>Mobile</option>
+													<option>Writing</option>
+													<option>Design</option>
+													<option>Data Entry</option>
+													<option>Product Sourcing & Manufacturing</option>
+													<option>Sales & marketing</option>
+													<option>Business, Accounting & Legal</option>
+													<option>Local Jobs & Services</option>
+													<option>Customized (Select skills manually)</option>
+													<option>Select a category of work (optional)</option>
+												</select>
+											</div>
+											<input type="submit" value="Post Project Now" id="post_submit">
+											<p>By clicking 'Post Project Now', you are indicating that you have read and agree to the <span>Terms &amp; Conditions</span> and <span>Privacy Policy</span></p>
+										</div>
+										<div class="clr"></div>
+									</div>
+								</form>
+							</div>
+							<div class="col-sm-4 col-sm-offset-1 body_job_posting_sidebar">
+								<div class="posting_sidebar">
+									<div class="post_sidebar_title">It's free to Post a Project!</div>
+									<div class="post_sidebar_content">
+										<div class="post_content_line">
+											<i class="fa fa-check" aria-hidden="true"></i>
+											<p>Get bids from skilled freelancers in minutes.</p>
+										</div>
+										<div class="post_content_line">
+											<i class="fa fa-check" aria-hidden="true"></i>
+											<p>View Sparkingjobs profiles and feedback, then instantly chat with them!</p>
+										</div>
+										<div class="post_content_line">
+											<i class="fa fa-check" aria-hidden="true"></i>
+											<p>With only a 3%* commission fee, your favorite Sparkingjobs can start working for you today!</p>
+										</div>
+										<div class="post_content_line">
+											<i class="fa fa-check" aria-hidden="true"></i>
+											<p>Pay the Sparkingjobs once you're 100% satisfied. *Minimum fees may apply.</p>
+										</div>
+										<div class="post_content_line">
+											<i class="fa fa-check no_opacity" aria-hidden="true"></i>
+											<span>Learn More</span>
+										</div>
+									</div>
+								</div>
+								<div class="posting_sidebar">
+									<img src="assets/images/post_video.png" alt="">
+								</div>
+								<div class="posting_sidebar">
+									<p class="p_content_sidebar_post">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been indus-try's standard dummy text ever sincer the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typeset-ting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only</p>
+								</div>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id="body_content_bot">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-6 body_content_bot_left">
+								<p>0</p>
+								<p>Registered Users</p>
+							</div>
+							<div class="col-sm-6 body_content_bot_right">
+								<p>0</p>
+								<p>Total Jobs Posted</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<div class="container-fluid">
+			<div class="row">
+				<div id="footer_content">
+					<div class="container">
+						<div class="row footer_content_top">
+							<div class="col-sm-2">
+								<h3>NETWORKS</h3>
+								<p>Projects</p>
+								<p>Contests</p>
+								<p>Sitemap</p>
+								<p>Projects Directory</p>
+								<p>User Directory</p>
+								<p>Freelancer Local</p>
+								<p>Services</p>
+								<p>Escrow</p>
+								<p>Warrior Forum</p>
+								<p>Freenmarket</p>
+							</div>
+							<div class="col-sm-2">
+								<h3>ABOUT</h3>
+								<p>About us </p>
+								<p>How it Works</p>
+								<p>Team</p>
+								<p>Mobile App</p>
+								<p>Desktop App</p>
+								<p>Security</p>
+								<p>Report Bug</p>
+								<p>Fees & Charges</p>
+								<p>Investor</p>
+								<p>Quotes</p>
+							</div>
+							<div class="col-sm-2">
+								<h3>PRESS</h3>
+								<p>In the News</p>
+								<p>Press Releases</p>
+								<p>Awards</p>
+								<p>Testimonials</p>
+								<p>Timeline</p>
+							</div>
+							<div class="col-sm-2">
+								<h3>GET IN TOUCH</h3>
+								<p>Get support</p>
+								<p>Advertise with Us</p>
+								<p>Careers</p>
+								<p>Community</p>
+								<p>Affiliate Program</p>
+								<p>Merchandise</p>
+								<p>Contact Us</p>
+							</div>
+							<div class="col-sm-4 footer_app_store">
+								<img src="assets/images/footer_image_1.png" alt="">
+								<img src="assets/images/footer_image_2.png" alt="">
+								<p>Download the SparkingJobs App</p>
+								<p>Connect with talented freelancers & manage your project wherever you are on the go.</p>
+							</div>
+							<div class="col-sm-12"><div class="footer_line"></div></div>
+							<div class="col-sm-7">
+								<div class="copyright_top">
+									<span>Privacy Policy</span>
+									<i>|</i>
+									<span>Terms and Conditions</span>
+									<i>|</i>
+									<span>Copyright Infringement Policy</span>
+									<i>|</i>
+									<span>Code of Conduct</span>
+								</div>
+								<div class="copyright_bot">
+									<p>Sparkingjobs <i class="fa fa-registered" aria-hidden="true"></i> is a registered Trademark of sparking jobs Technology Pty Limited</p>
+									<p>Sparkingjobs <i class="fa fa-copyright" aria-hidden="true"></i> 2016 sparkingjobs Technology Pty Limited ACN 123456</p>
+								</div>
+							</div>
+							<div class="col-sm-5 footer_link">
+								<i class="fa fa-facebook" aria-hidden="true"></i>
+								<i class="fa fa-twitter" aria-hidden="true"></i>
+								<i class="fa fa-google-plus" aria-hidden="true"></i>
+								<i class="fa fa-youtube" aria-hidden="true"></i>
+								<i class="fa fa-rss" aria-hidden="true"></i>
+								<i class="fa fa-camera-retro" aria-hidden="true"></i>
+							</div>
+							<div class="col-sm-12 footer_logo">
+								<img src="assets/images/header_logo.png" alt="Sparking Jobs">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script type="text/javascript" src="assets/js/custom.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+
+</body>
+</html>
