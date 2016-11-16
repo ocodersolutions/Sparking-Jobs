0c897718b59e1ce6e0e565a0927797c2851f29c0
Nguyen Sanh Toan <stoannguyen2205@gmail.com>
2016-11-16 08:27:10 +0700

    create sourch code

---
+++ index.php
@@ -0,0 +1,471 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title>Sparking Jobs</title>
+
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+	<script type="text/javascript" src="assets/js/jquery-1.12.3.min.js"></script>	
+
+</head>
+<body>
+	<div id="header">
+		<div class="container-fluid">
+			<div class="row">
+				<div id="header_top">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-12">
+								<i class="fa fa-bars" aria-hidden="true"></i>
+								<div class="header_logo">
+									<a href="#"><img src="assets/images/header_logo.png" alt="Sparking Jobs"></a>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+				<div id="header_bot">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-12 header_bot_one">
+								<div class="col-sm-5 col-sm-offset-1 header_bot_one_left">
+									<form action="">
+										<input type="text" name="search_header" placeholder="Find Services">
+										<button type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+									</form>
+								</div>
+								<div class="col-sm-5 header_bot_one_right">
+									<ul>
+										<li>Community</li>
+										<li>
+											<i class="fa fa-signal" aria-hidden="true"></i>
+											<i class="fa fa-bell-o" aria-hidden="true"></i>
+											<i class="fa fa-commenting-o" aria-hidden="true"></i>
+										</li>
+										<li>
+											<i class="fa fa-user-o" aria-hidden="true"></i>
+											<span>Franknava</span>
+										</li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-sm-12 header_bot_two">
+								<div class="row header_menu">
+									<div class="col-sm-2 header_menu_op header_menu_op_website">
+										<div>
+											<i class="fa fa-desktop" aria-hidden="true"></i>
+											<span>Websites, IT & Software</span>
+										</div>
+									</div>
+									<div class="col-sm-2 header_menu_op header_menu_op_mobile">
+										<div>
+											<i class="fa fa-mobile" aria-hidden="true"></i>
+											<span>Mobile Phones & Computing</span>
+										</div>
+									</div>
+									<div class="col-sm-2 header_menu_op header_menu_op_design">
+										<div>
+											<i class="fa fa-archive" aria-hidden="true"></i>
+											<span>Design, Media & Architecture</span>
+										</div>
+									</div>
+									<div class="col-sm-2 header_menu_op header_menu_op_writing">
+										<div>
+											<i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+											<span>Writing & Content</span>
+										</div>
+									</div>
+									<div class="col-sm-2 header_menu_op header_menu_op_data">
+										<div>
+											<i class="fa fa-calendar-o" aria-hidden="true"></i>
+											<span>Data Entry & Admin</span>
+										</div>
+									</div>
+									<div class="col-sm-2 header_menu_op header_menu_op_local">
+										<div>
+											<i class="fa fa-server" aria-hidden="true"></i>
+											<span>Local Jobs & Services</span>
+										</div>
+									</div>
+									<div class="col-sm-2 header_menu_op header_menu_op_sales">
+										<div>
+											<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+											<span>Sales & Marketing</span>
+										</div>
+									</div>
+									<div class="col-sm-2 header_menu_op header_menu_op_translation">
+										<div>
+											<i class="fa fa-globe" aria-hidden="true"></i>
+											<span>Translation & Languages</span>
+										</div>
+									</div>
+									<div class="col-sm-2 header_menu_op header_menu_op_product">
+										<div>
+											<i class="fa fa-tags" aria-hidden="true"></i>
+											<span>Product Sourcing & Manufacturing</span>
+										</div>
+									</div>
+									<div class="col-sm-2 header_menu_op header_menu_op_engineering">
+										<div>
+											<i class="fa fa-flask" aria-hidden="true"></i>
+											<span>Engineering & Science</span>
+										</div>
+									</div>
+									<div class="col-sm-2 header_menu_op header_menu_op_business">
+										<div>
+											<i class="fa fa-handshake-o" aria-hidden="true"></i>
+											<span>Business Services</span>
+										</div>
+									</div>
+									<div class="col-sm-2 header_menu_op header_menu_op_other">
+										<div>
+											<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+											<span>Other</span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+	</div>
+	<div id="body_content">
+		<div class="container-fluid">
+			<div class="row">
+				<div id="body_content_top">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-3 body_sidebar">
+								<div class="body_sidebar_one">
+									<h2>Hi, Franknava</h2>
+									<p>Request the service you are looking for.</p>
+									<span>Post a Request</span>
+								</div>
+								<div class="body_sidebar_two">
+									<h3>BUY IT AGAIN</h3>
+									<img src="assets/images/buy_it_again.jpg" alt="">
+									<div>
+										<p>design</p>
+										<p>PROFESIONAL</p>
+										<p>business card</p>
+										<p class="sibar_two_p_color">C$13.38 WITH</p>
+										<p class="sibar_two_p_color">EXTRAS</p>
+									</div>
+								</div>
+								<div class="body_sidebar_three">
+									<h3>RECENTLY VIEWED</h3>
+									<div class="recently_view_item">
+										<img src="assets/images/recently_view_1.jpg" alt="">
+										<p>write Original Web Content</p>
+										<div class="clr"></div>
+									</div>
+									<div class="recently_view_item">
+										<img src="assets/images/recently_view_2.jpg" alt="">
+										<p>do perfect content writing for you</p>
+										<div class="clr"></div>
+									</div>
+									<div class="recently_view_item">
+										<img src="assets/images/recently_view_3.jpg" alt="">
+										<p>write research, creative and engaging SEO</p>
+										<div class="clr"></div>
+									</div>
+									<div class="recently_view_item">
+										<img src="assets/images/recently_view_4.jpg" alt="">
+										<p>offer best artice and well researched cotent</p>
+										<div class="clr"></div>
+									</div>
+									<div class="recently_view_item">
+										<img src="assets/images/recently_view_5.jpg" alt="">
+										<p>write website content for you</p>
+										<div class="clr"></div>
+									</div>
+									<div class="recently_view_item">
+										<img src="assets/images/recently_view_1.jpg" alt="">
+										<p>write Original Web Content</p>
+										<div class="clr"></div>
+									</div>
+									<div class="recently_view_item">
+										<img src="assets/images/recently_view_2.jpg" alt="">
+										<p>do perfect content writing for you</p>
+										<div class="clr"></div>
+									</div>
+									<div class="recently_view_item">
+										<img src="assets/images/recently_view_3.jpg" alt="">
+										<p>write research, creative and engaging SEO</p>
+										<div class="clr"></div>
+									</div>
+									<div class="recently_view_item">
+										<img src="assets/images/recently_view_4.jpg" alt="">
+										<p>offer best artice and well researched cotent</p>
+										<div class="clr"></div>
+									</div>
+									<div class="recently_view_item">
+										<img src="assets/images/recently_view_5.jpg" alt="">
+										<p>write website content for you</p>
+										<div class="clr"></div>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-9 body_main_content">
+								<div class="body_content_slider">
+									<img src="assets/images/slider.jpg" alt="">
+								</div>
+								<div class="row body_content_product">
+									<div class="col-sm-3 product_item">
+										<div class="product_item_content">
+											<img src="assets/images/product_item_1.jpg" alt="">
+											<p>I will write Complete WEBSITE content</p>
+											<div>
+												<i class="fa fa-bars" aria-hidden="true"></i>
+												<i class="fa fa-heart" aria-hidden="true"></i>
+												<span>Make an Offer</span>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-3 product_item">
+										<div class="product_item_content">
+											<img src="assets/images/product_item_2.jpg" alt="">
+											<p>I will write Unique WEBSITE content</p>
+											<div>
+												<i class="fa fa-bars" aria-hidden="true"></i>
+												<i class="fa fa-heart" aria-hidden="true"></i>
+												<span>Make an Offer</span>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-3 product_item">
+										<div class="product_item_content">
+											<img src="assets/images/product_item_3.jpg" alt="">
+											<p>I will write Powerful WEBSITE content</p>
+											<div>
+												<i class="fa fa-bars" aria-hidden="true"></i>
+												<i class="fa fa-heart" aria-hidden="true"></i>
+												<span>Make an Offer</span>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-3 product_item">
+										<div class="product_item_content">
+											<img src="assets/images/product_item_4.jpg" alt="">
+											<p>I will write Original and effective WEBSITE content</p>
+											<div>
+												<i class="fa fa-bars" aria-hidden="true"></i>
+												<i class="fa fa-heart" aria-hidden="true"></i>
+												<span>Make an Offer</span>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-3 product_item">
+										<div class="product_item_content">
+											<img src="assets/images/product_item_1.jpg" alt="">
+											<p>I will write Complete WEBSITE content</p>
+											<div>
+												<i class="fa fa-bars" aria-hidden="true"></i>
+												<i class="fa fa-heart" aria-hidden="true"></i>
+												<span>Make an Offer</span>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-3 product_item">
+										<div class="product_item_content">
+											<img src="assets/images/product_item_2.jpg" alt="">
+											<p>I will write Unique WEBSITE content</p>
+											<div>
+												<i class="fa fa-bars" aria-hidden="true"></i>
+												<i class="fa fa-heart" aria-hidden="true"></i>
+												<span>Make an Offer</span>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-3 product_item">
+										<div class="product_item_content">
+											<img src="assets/images/product_item_3.jpg" alt="">
+											<p>I will write Powerful WEBSITE content</p>
+											<div>
+												<i class="fa fa-bars" aria-hidden="true"></i>
+												<i class="fa fa-heart" aria-hidden="true"></i>
+												<span>Make an Offer</span>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-3 product_item">
+										<div class="product_item_content">
+											<img src="assets/images/product_item_4.jpg" alt="">
+											<p>I will write Original and effective WEBSITE content</p>
+											<div>
+												<i class="fa fa-bars" aria-hidden="true"></i>
+												<i class="fa fa-heart" aria-hidden="true"></i>
+												<span>Make an Offer</span>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-3 product_item">
+										<div class="product_item_content">
+											<img src="assets/images/product_item_1.jpg" alt="">
+											<p>I will write Complete WEBSITE content</p>
+											<div>
+												<i class="fa fa-bars" aria-hidden="true"></i>
+												<i class="fa fa-heart" aria-hidden="true"></i>
+												<span>Make an Offer</span>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-3 product_item">
+										<div class="product_item_content">
+											<img src="assets/images/product_item_2.jpg" alt="">
+											<p>I will write Unique WEBSITE content</p>
+											<div>
+												<i class="fa fa-bars" aria-hidden="true"></i>
+												<i class="fa fa-heart" aria-hidden="true"></i>
+												<span>Make an Offer</span>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-3 product_item">
+										<div class="product_item_content">
+											<img src="assets/images/product_item_3.jpg" alt="">
+											<p>I will write Powerful WEBSITE content</p>
+											<div>
+												<i class="fa fa-bars" aria-hidden="true"></i>
+												<i class="fa fa-heart" aria-hidden="true"></i>
+												<span>Make an Offer</span>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-3 product_item">
+										<div class="product_item_content">
+											<img src="assets/images/product_item_4.jpg" alt="">
+											<p>I will write Original and effective WEBSITE content</p>
+											<div>
+												<i class="fa fa-bars" aria-hidden="true"></i>
+												<i class="fa fa-heart" aria-hidden="true"></i>
+												<span>Make an Offer</span>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id="body_content_bot">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-6 body_content_bot_left">
+								<p>0</p>
+								<p>Registered Users</p>
+							</div>
+							<div class="col-sm-6 body_content_bot_right">
+								<p>0</p>
+								<p>Total Jobs Posted</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<div class="container-fluid">
+			<div class="row">
+				<div id="footer_content">
+					<div class="container">
+						<div class="row footer_content_top">
+							<div class="col-sm-2">
+								<h3>NETWORKS</h3>
+								<p>Projects</p>
+								<p>Contests</p>
+								<p>Sitemap</p>
+								<p>Projects Directory</p>
+								<p>User Directory</p>
+								<p>Freelancer Local</p>
+								<p>Services</p>
+								<p>Escrow</p>
+								<p>Warrior Forum</p>
+								<p>Freenmarket</p>
+							</div>
+							<div class="col-sm-2">
+								<h3>ABOUT</h3>
+								<p>About us </p>
+								<p>How it Works</p>
+								<p>Team</p>
+								<p>Mobile App</p>
+								<p>Desktop App</p>
+								<p>Security</p>
+								<p>Report Bug</p>
+								<p>Fees & Charges</p>
+								<p>Investor</p>
+								<p>Quotes</p>
+							</div>
+							<div class="col-sm-2">
+								<h3>PRESS</h3>
+								<p>In the News</p>
+								<p>Press Releases</p>
+								<p>Awards</p>
+								<p>Testimonials</p>
+								<p>Timeline</p>
+							</div>
+							<div class="col-sm-2">
+								<h3>GET IN TOUCH</h3>
+								<p>Get support</p>
+								<p>Advertise with Us</p>
+								<p>Careers</p>
+								<p>Community</p>
+								<p>Affiliate Program</p>
+								<p>Merchandise</p>
+								<p>Contact Us</p>
+							</div>
+							<div class="col-sm-4 footer_app_store">
+								<img src="assets/images/footer_image_1.png" alt="">
+								<img src="assets/images/footer_image_2.png" alt="">
+								<p>Download the SparkingJobs App</p>
+								<p>Connect with talented freelancers & manage your project wherever you are on the go.</p>
+							</div>
+							<div class="col-sm-12"><div class="footer_line"></div></div>
+							<div class="col-sm-7">
+								<div class="copyright_top">
+									<span>Privacy Policy</span>
+									<i>|</i>
+									<span>Terms and Conditions</span>
+									<i>|</i>
+									<span>Copyright Infringement Policy</span>
+									<i>|</i>
+									<span>Code of Conduct</span>
+								</div>
+								<div class="copyright_bot">
+									<p>Sparkingjobs <i class="fa fa-registered" aria-hidden="true"></i> is a registered Trademark of sparking jobs Technology Pty Limited</p>
+									<p>Sparkingjobs <i class="fa fa-copyright" aria-hidden="true"></i> 2016 sparkingjobs Technology Pty Limited ACN 123456</p>
+								</div>
+							</div>
+							<div class="col-sm-5 footer_link">
+								<i class="fa fa-facebook" aria-hidden="true"></i>
+								<i class="fa fa-twitter" aria-hidden="true"></i>
+								<i class="fa fa-google-plus" aria-hidden="true"></i>
+								<i class="fa fa-youtube" aria-hidden="true"></i>
+								<i class="fa fa-rss" aria-hidden="true"></i>
+								<i class="fa fa-camera-retro" aria-hidden="true"></i>
+							</div>
+							<div class="col-sm-12 footer_logo">
+								<img src="assets/images/header_logo.png" alt="Sparking Jobs">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script type="text/javascript" src="assets/js/custom.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+
+</body>
+</html>
