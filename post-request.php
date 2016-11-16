7bea275134620ce375ab61f2b822ad087316faa1
Nguyen Sanh Toan <stoannguyen2205@gmail.com>
2016-11-16 17:19:40 +0700

    create post-request-page

---
+++ post-request.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<title>Sparking Jobs</title>
+
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+	<script type="text/javascript" src="assets/js/jquery-1.12.3.min.js"></script>	
+
+</head>
+<body>
+	<div id="header">
+		<div class="container-fluid">
+			<div class="row">
+				<div id="header_top">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-12">
+								<i class="fa fa-bars" aria-hidden="true"></i>
+								<div class="header_logo">
+									<a href="#"><img src="assets/images/header_logo.png" alt="Sparking Jobs"></a>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="body_content">
+		<div class="container-fluid">
+			<div class="row">
+				<div id="body_content_top">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-8 post_request_content">
+								<div class="select_allproposal">
+									<span>All Proposals</span>
+									<span>Online</span>
+								</div>
+								<div class="row body_post_request">
+									<div class="col-sm-6 post_request_item">
+										<div class="pr_item_content">
+											<div class="pr_item_content_top">
+												<img src="assets/images/post_request_item_1.jpg" alt="">
+												<p class="post_request_item_title">Lorem Ipsum</p>
+												<p class="post_request_item_content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been indus-try's standard dummy text ever sincer the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+											</div>
+											<div class="pr_item_content_left">
+												<div class="post_item_icon">
+													<img src="assets/images/post_item_icon_1.jpg" alt="">
+													<img src="assets/images/post_item_icon_2.jpg" alt="">
+													<img src="assets/images/post_item_icon_3.jpg" alt="">
+													<img src="assets/images/post_item_icon_4.jpg" alt="">
+												</div>
+												<div class="make_an_offer">Make an Offer</div>
+												<div class="live_chat"><i class="fa fa-circle" aria-hidden="true"></i><span>Live Chat</span></div>
+											</div>
+											<div class="pr_item_content_right">
+												<b>$ 244</b>
+												<div class="pr_start">
+													<span>5.0</span>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+												</div>
+												<div class="pr_money">
+													<span>3.5</span>
+													<img src="assets/images/pr_money.jpg" alt="">
+												</div>
+												<p>10 Reviews</p>
+												<p>100% Jobs Completed</p>
+											</div>
+											<div class="clr"></div>
+										</div>
+									</div>
+									<div class="col-sm-6 post_request_item">
+										<div class="pr_item_content">
+											<div class="pr_item_content_top">
+												<img src="assets/images/post_request_item_2.jpg" alt="">
+												<p class="post_request_item_title">Lorem Ipsum</p>
+												<p class="post_request_item_content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been indus-try's standard dummy text ever sincer the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+											</div>
+											<div class="pr_item_content_left">
+												<div class="post_item_icon">
+													<img src="assets/images/post_item_icon_1.jpg" alt="">
+													<img src="assets/images/post_item_icon_2.jpg" alt="">
+													<img src="assets/images/post_item_icon_3.jpg" alt="">
+													<img src="assets/images/post_item_icon_4.jpg" alt="">
+												</div>
+												<div class="make_an_offer">Make an Offer</div>
+												<div class="live_chat"><i class="fa fa-circle" aria-hidden="true"></i><span>Live Chat</span></div>
+											</div>
+											<div class="pr_item_content_right">
+												<b>$ 244</b>
+												<div class="pr_start">
+													<span>5.0</span>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+												</div>
+												<div class="pr_money">
+													<span>3.5</span>
+													<img src="assets/images/pr_money.jpg" alt="">
+												</div>
+												<p>10 Reviews</p>
+												<p>100% Jobs Completed</p>
+											</div>
+											<div class="clr"></div>
+										</div>
+									</div>
+									<div class="col-sm-6 post_request_item">
+										<div class="pr_item_content">
+											<div class="pr_item_content_top">
+												<img src="assets/images/post_request_item_1.jpg" alt="">
+												<p class="post_request_item_title">Lorem Ipsum</p>
+												<p class="post_request_item_content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been indus-try's standard dummy text ever sincer the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+											</div>
+											<div class="pr_item_content_left">
+												<div class="post_item_icon">
+													<img src="assets/images/post_item_icon_1.jpg" alt="">
+													<img src="assets/images/post_item_icon_2.jpg" alt="">
+													<img src="assets/images/post_item_icon_3.jpg" alt="">
+													<img src="assets/images/post_item_icon_4.jpg" alt="">
+												</div>
+												<div class="make_an_offer">Make an Offer</div>
+												<div class="live_chat"><i class="fa fa-circle" aria-hidden="true"></i><span>Live Chat</span></div>
+											</div>
+											<div class="pr_item_content_right">
+												<b>$ 244</b>
+												<div class="pr_start">
+													<span>5.0</span>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+												</div>
+												<div class="pr_money">
+													<span>3.5</span>
+													<img src="assets/images/pr_money.jpg" alt="">
+												</div>
+												<p>10 Reviews</p>
+												<p>100% Jobs Completed</p>
+											</div>
+											<div class="clr"></div>
+										</div>
+									</div>
+									<div class="col-sm-6 post_request_item">
+										<div class="pr_item_content">
+											<div class="pr_item_content_top">
+												<img src="assets/images/post_request_item_2.jpg" alt="">
+												<p class="post_request_item_title">Lorem Ipsum</p>
+												<p class="post_request_item_content">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been indus-try's standard dummy text ever sincer the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+											</div>
+											<div class="pr_item_content_left">
+												<div class="post_item_icon">
+													<img src="assets/images/post_item_icon_1.jpg" alt="">
+													<img src="assets/images/post_item_icon_2.jpg" alt="">
+													<img src="assets/images/post_item_icon_3.jpg" alt="">
+													<img src="assets/images/post_item_icon_4.jpg" alt="">
+												</div>
+												<div class="make_an_offer">Make an Offer</div>
+												<div class="live_chat"><i class="fa fa-circle" aria-hidden="true"></i><span>Live Chat</span></div>
+											</div>
+											<div class="pr_item_content_right">
+												<b>$ 244</b>
+												<div class="pr_start">
+													<span>5.0</span>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+													<i class="fa fa-star" aria-hidden="true"></i>
+												</div>
+												<div class="pr_money">
+													<span>3.5</span>
+													<img src="assets/images/pr_money.jpg" alt="">
+												</div>
+												<p>10 Reviews</p>
+												<p>100% Jobs Completed</p>
+											</div>
+											<div class="clr"></div>
+										</div>
+									</div>
+								</div>
+								<div class="request_pagination">
+									<ul class="pagination">
+										<li><a href="#">First</a></li>
+										<li><a href="#">Previous</a></li>
+										<li><a href="#">1</a></li>
+										<li><a class="" href="#">2</a></li>
+										<li><a href="#">3</a></li>
+										<li><a href="#">4</a></li>
+										<li><a href="#">5</a></li>
+										<li><a href="#">6</a></li>
+										<li><a href="#">7</a></li>
+										<li><a href="#" class="red_color_pg">Next</a></li>
+										<li><a href="#" class="red_color_pg">Last</a></li>
+									</ul>
+								</div>
+							</div>
+							<div class="col-sm-4 post_request_sidebar">
+								<div class="post_request_sidebar_content">
+									<div class="pr_sidebar_one">
+										<span class="pr_filter">Filters</span>
+										<span class="pr_save_filter">Save Filters</span>
+										<div class="clr"></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div id="body_content_bot">
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-6 body_content_bot_left">
+								<p>0</p>
+								<p>Registered Users</p>
+							</div>
+							<div class="col-sm-6 body_content_bot_right">
+								<p>0</p>
+								<p>Total Jobs Posted</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="footer">
+		<div class="container-fluid">
+			<div class="row">
+				<div id="footer_content">
+					<div class="container">
+						<div class="row footer_content_top">
+							<div class="col-sm-2">
+								<h3>NETWORKS</h3>
+								<p>Projects</p>
+								<p>Contests</p>
+								<p>Sitemap</p>
+								<p>Projects Directory</p>
+								<p>User Directory</p>
+								<p>Freelancer Local</p>
+								<p>Services</p>
+								<p>Escrow</p>
+								<p>Warrior Forum</p>
+								<p>Freenmarket</p>
+							</div>
+							<div class="col-sm-2">
+								<h3>ABOUT</h3>
+								<p>About us </p>
+								<p>How it Works</p>
+								<p>Team</p>
+								<p>Mobile App</p>
+								<p>Desktop App</p>
+								<p>Security</p>
+								<p>Report Bug</p>
+								<p>Fees & Charges</p>
+								<p>Investor</p>
+								<p>Quotes</p>
+							</div>
+							<div class="col-sm-2">
+								<h3>PRESS</h3>
+								<p>In the News</p>
+								<p>Press Releases</p>
+								<p>Awards</p>
+								<p>Testimonials</p>
+								<p>Timeline</p>
+							</div>
+							<div class="col-sm-2">
+								<h3>GET IN TOUCH</h3>
+								<p>Get support</p>
+								<p>Advertise with Us</p>
+								<p>Careers</p>
+								<p>Community</p>
+								<p>Affiliate Program</p>
+								<p>Merchandise</p>
+								<p>Contact Us</p>
+							</div>
+							<div class="col-sm-4 footer_app_store">
+								<img src="assets/images/footer_image_1.png" alt="">
+								<img src="assets/images/footer_image_2.png" alt="">
+								<p>Download the SparkingJobs App</p>
+								<p>Connect with talented freelancers & manage your project wherever you are on the go.</p>
+							</div>
+							<div class="col-sm-12"><div class="footer_line"></div></div>
+							<div class="col-sm-7">
+								<div class="copyright_top">
+									<span>Privacy Policy</span>
+									<i>|</i>
+									<span>Terms and Conditions</span>
+									<i>|</i>
+									<span>Copyright Infringement Policy</span>
+									<i>|</i>
+									<span>Code of Conduct</span>
+								</div>
+								<div class="copyright_bot">
+									<p>Sparkingjobs <i class="fa fa-registered" aria-hidden="true"></i> is a registered Trademark of sparking jobs Technology Pty Limited</p>
+									<p>Sparkingjobs <i class="fa fa-copyright" aria-hidden="true"></i> 2016 sparkingjobs Technology Pty Limited ACN 123456</p>
+								</div>
+							</div>
+							<div class="col-sm-5 footer_link">
+								<i class="fa fa-facebook" aria-hidden="true"></i>
+								<i class="fa fa-twitter" aria-hidden="true"></i>
+								<i class="fa fa-google-plus" aria-hidden="true"></i>
+								<i class="fa fa-youtube" aria-hidden="true"></i>
+								<i class="fa fa-rss" aria-hidden="true"></i>
+								<i class="fa fa-camera-retro" aria-hidden="true"></i>
+							</div>
+							<div class="col-sm-12 footer_logo">
+								<img src="assets/images/header_logo.png" alt="Sparking Jobs">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<script type="text/javascript" src="assets/js/custom.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+
+</body>
+</html>
