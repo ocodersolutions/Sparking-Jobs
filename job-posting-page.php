cf8055e695bc67cdf68c6aa3c65650c18990cbe6
Nguyen Sanh Toan <stoannguyen2205@gmail.com>
2016-11-19 11:09:35 +0700

    fix job-posting-page

--- job-posting-page.php
+++ job-posting-page.php
@@ -33,7 +33,7 @@
 			</div>
 		</div>
 	</div>
-	<div id="body_content">
+	<div id="body_content" class="job_posting_page">
 		<div class="container-fluid">
 			<div class="row">
 				<div id="body_content_top">
@@ -168,112 +168,111 @@
 						</div>
 					</div>
 				</div>
-				<div id="body_content_bot">
-					<div class="container">
-						<div class="row">
-							<div class="col-sm-6 body_content_bot_left">
-								<p>0</p>
-								<p>Registered Users</p>
-							</div>
-							<div class="col-sm-6 body_content_bot_right">
-								<p>0</p>
-								<p>Total Jobs Posted</p>
-							</div>
-						</div>
-					</div>
-				</div>
 			</div>
 		</div>
 	</div>
-	<div id="footer">
-		<div class="container-fluid">
-			<div class="row">
-				<div id="footer_content">
-					<div class="container">
-						<div class="row footer_content_top">
-							<div class="col-sm-2">
-								<h3>NETWORKS</h3>
-								<p>Projects</p>
-								<p>Contests</p>
-								<p>Sitemap</p>
-								<p>Projects Directory</p>
-								<p>User Directory</p>
-								<p>Freelancer Local</p>
-								<p>Services</p>
-								<p>Escrow</p>
-								<p>Warrior Forum</p>
-								<p>Freenmarket</p>
-							</div>
-							<div class="col-sm-2">
-								<h3>ABOUT</h3>
-								<p>About us </p>
-								<p>How it Works</p>
-								<p>Team</p>
-								<p>Mobile App</p>
-								<p>Desktop App</p>
-								<p>Security</p>
-								<p>Report Bug</p>
-								<p>Fees & Charges</p>
-								<p>Investor</p>
-								<p>Quotes</p>
-							</div>
-							<div class="col-sm-2">
-								<h3>PRESS</h3>
-								<p>In the News</p>
-								<p>Press Releases</p>
-								<p>Awards</p>
-								<p>Testimonials</p>
-								<p>Timeline</p>
-							</div>
-							<div class="col-sm-2">
-								<h3>GET IN TOUCH</h3>
-								<p>Get support</p>
-								<p>Advertise with Us</p>
-								<p>Careers</p>
-								<p>Community</p>
-								<p>Affiliate Program</p>
-								<p>Merchandise</p>
-								<p>Contact Us</p>
-							</div>
-							<div class="col-sm-4 footer_app_store">
-								<img src="assets/images/footer_image_1.png" alt="">
-								<img src="assets/images/footer_image_2.png" alt="">
-								<p>Download the SparkingJobs App</p>
-								<p>Connect with talented freelancers & manage your project wherever you are on the go.</p>
-							</div>
-							<div class="col-sm-12"><div class="footer_line"></div></div>
-							<div class="col-sm-7">
-								<div class="copyright_top">
-									<span>Privacy Policy</span>
-									<i>|</i>
-									<span>Terms and Conditions</span>
-									<i>|</i>
-									<span>Copyright Infringement Policy</span>
-									<i>|</i>
-									<span>Code of Conduct</span>
-								</div>
-								<div class="copyright_bot">
-									<p>Sparkingjobs <i class="fa fa-registered" aria-hidden="true"></i> is a registered Trademark of sparking jobs Technology Pty Limited</p>
-									<p>Sparkingjobs <i class="fa fa-copyright" aria-hidden="true"></i> 2016 sparkingjobs Technology Pty Limited ACN 123456</p>
-								</div>
-							</div>
-							<div class="col-sm-5 footer_link">
-								<i class="fa fa-facebook" aria-hidden="true"></i>
-								<i class="fa fa-twitter" aria-hidden="true"></i>
-								<i class="fa fa-google-plus" aria-hidden="true"></i>
-								<i class="fa fa-youtube" aria-hidden="true"></i>
-								<i class="fa fa-rss" aria-hidden="true"></i>
-								<i class="fa fa-camera-retro" aria-hidden="true"></i>
-							</div>
-							<div class="col-sm-12 footer_logo">
-								<img src="assets/images/header_logo.png" alt="Sparking Jobs">
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+	<div id="footer" class="milestone_footer">
+    <div class="container-fluid">
+      <div class="row">
+        <div id="footer_public">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-6 body_content_bot_left">
+                <p>0</p>
+                <p>Registered Users</p>
+              </div>
+              <div class="col-sm-6 body_content_bot_right">
+                <p>0</p>
+                <p>Total Jobs Posted</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="footer_content">
+          <div class="container">
+            <div class="row footer_content_top">
+              <div class="col-sm-2">
+                <h3>NETWORKS</h3>
+                <p>Projects</p>
+                <p>Contests</p>
+                <p>Sitemap</p>
+                <p>Projects Directory</p>
+                <p>User Directory</p>
+                <p>Freelancer Local</p>
+                <p>Services</p>
+                <p>Escrow</p>
+                <p>Warrior Forum</p>
+                <p>Freenmarket</p>
+              </div>
+              <div class="col-sm-2">
+                <h3>ABOUT</h3>
+                <p>About us </p>
+                <p>How it Works</p>
+                <p>Team</p>
+                <p>Mobile App</p>
+                <p>Desktop App</p>
+                <p>Security</p>
+                <p>Report Bug</p>
+                <p>Fees & Charges</p>
+                <p>Investor</p>
+                <p>Quotes</p>
+              </div>
+              <div class="col-sm-2">
+                <h3>PRESS</h3>
+                <p>In the News</p>
+                <p>Press Releases</p>
+                <p>Awards</p>
+                <p>Testimonials</p>
+                <p>Timeline</p>
+              </div>
+              <div class="col-sm-2">
+                <h3>GET IN TOUCH</h3>
+                <p>Get support</p>
+                <p>Advertise with Us</p>
+                <p>Careers</p>
+                <p>Community</p>
+                <p>Affiliate Program</p>
+                <p>Merchandise</p>
+                <p>Contact Us</p>
+              </div>
+              <div class="col-sm-4 footer_app_store">
+                <img src="assets/images/footer_image_1.png" alt="">
+                <img src="assets/images/footer_image_2.png" alt="">
+                <p>Download the SparkingJobs App</p>
+                <p>Connect with talented freelancers & manage your project wherever you are on the go.</p>
+              </div>
+              <div class="col-sm-12"><div class="footer_line"></div></div>
+              <div class="col-sm-7">
+                <div class="copyright_top">
+                  <span>Privacy Policy</span>
+                  <i>|</i>
+                  <span>Terms and Conditions</span>
+                  <i>|</i>
+                  <span>Copyright Infringement Policy</span>
+                  <i>|</i>
+                  <span>Code of Conduct</span>
+                </div>
+                <div class="copyright_bot">
+                  <p>Sparkingjobs <i class="fa fa-registered" aria-hidden="true"></i> is a registered Trademark of sparking jobs Technology Pty Limited</p>
+                  <p>Sparkingjobs <i class="fa fa-copyright" aria-hidden="true"></i> 2016 sparkingjobs Technology Pty Limited ACN 123456</p>
+                </div>
+              </div>
+              <div class="col-sm-5 footer_link">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+                <i class="fa fa-google-plus" aria-hidden="true"></i>
+                <i class="fa fa-linkedin" aria-hidden="true"></i>
+                <i class="fa fa-rss" aria-hidden="true"></i>
+              </div>
+              <div class="col-sm-12 footer_logo">
+                <img src="assets/images/header_logo.png" alt="Sparking Jobs">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
 	<script type="text/javascript" src="assets/js/custom.js"></script>
 	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
