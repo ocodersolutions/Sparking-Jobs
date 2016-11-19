5881614dd376838d47af79f321468e6496f33068
Nguyen Sanh Toan <stoannguyen2205@gmail.com>
2016-11-19 10:45:07 +0700

    create milestone page

---
+++ milestone.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+  <title>Sparking Jobs</title>
+
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/css/bootstrap-theme.min.css">
+  <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+  <script type="text/javascript" src="assets/js/jquery-1.12.3.min.js"></script> 
+
+</head>
+<body>
+  <div id="header">
+    <div class="container-fluid">
+      <div class="row">
+        <div id="header_top">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12">
+                <i class="fa fa-bars" aria-hidden="true"></i>
+                <div class="header_logo">
+                  <a href="#"><img src="assets/images/header_logo.png" alt="Sparking Jobs"></a>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="body_content" class="milestone-page">
+    <div class="container-fluid">
+      <div class="row">
+        <div id="add_body_top">
+          <div class="hb_milestone_one">
+            <div class="container">
+              <div class="row">
+                <div class="hb_milestone_one_content col-sm-12">
+                  <a href="">Home</a><span>></span><a href="">MyProjects</a><span>></span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="hb_milestone_two">
+            <div class="container">
+              <div class="row">
+                <div class="col-sm-6 hb_milestone_two_left">
+                  <h3>Project For Lorem Ipsum</h3>
+                </div>
+                <div class="col-sm-2 col-sm-offset-4">
+                  <div class="hb_milestone_tow_right">
+                    <button>Available Credit</button>
+                    <b>$350.00 USD</b>
+                    <p>Upgrade Project</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="body_milestone">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-9 body_milestone_content">
+                <div class="body_milestone_cone">
+                  <h3>Secure Payment <i class="fa fa-question" aria-hidden="true"></i></h3>
+                  <div class="secure_line">&nbsp;</div>
+                  <div class="secure_payment_point">
+                    <label for="">60%</label>
+                    <label for="">70%</label>
+                    <label for="">50%</label>
+                  </div>
+                  <div class="create_milestone">
+                    <p class="create_milestone_title">Create a Milestone</p>
+                    <div class="create_milestone_money">
+                      <div class="milestone_enter_money">
+                        <input type="text">
+                      </div>
+                      <input type="text" class="milestone_input_decription" placeholder="Enter a description">
+                      <p><span>Make a Deposit</span></p>
+                    </div>
+                  </div>
+                  <div class="milestone_refund">
+                    <div class="milestone_refund_left">
+                      <i class="fa fa-usd" aria-hidden="true"></i>
+                      <span>$120.00 USD</span>
+                      <i class="refund_depoist">Depoist</i>
+                    </div>
+                    <div class="milestone_refund_right">
+                      <span>Refund</span>
+                    </div>
+                    <div class="clr"></div>
+                  </div>
+                </div>
+                <div class="invoices_milestone">
+                  <h3>invoices</h3>
+                  <div class="invoices_table">
+                    <table>
+                      <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Invoice no.</th>
+                            <th>Invoice Amount</th>
+                            <th>Status</th>
+                            <th>Actions</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>06/12/2015</td>
+                          <td>25486</td>
+                          <td>$245.00</td>
+                          <td><span>Withdraw</span></td>
+                          <td><span>View</span></td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+                <div class="attached_milestone">
+                  <h3>No Files Attached</h3>
+                  <div>Upload</div>
+                  <p class="clr"></p>
+                </div>
+              </div>
+              <div class="col-sm-3 body_milestone_sidebar">
+                <div class="milestone_sidebar_content">
+                  <div class="milestone_sc_one">
+                    <img src="assets/images/milestone_sidebar_img.jpg" alt="">
+                    <div class="milestone_sc_one_right">
+                      <h4>What's a Milestone?</h4>
+                      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Lorem Ipsum is symply dummy text of the printing and typeseeting industry.</p>
+                    </div>
+                    <div class="clr"></div>
+                  </div>
+                  <div class="milestone_sc_two">
+                    <button>OPEN CHAT</button>
+                    <button>START NEW</button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="footer" class="milestone_footer">
+    <div class="container-fluid">
+      <div class="row">
+        <div id="footer_public">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-6 body_content_bot_left">
+                <p>0</p>
+                <p>Registered Users</p>
+              </div>
+              <div class="col-sm-6 body_content_bot_right">
+                <p>0</p>
+                <p>Total Jobs Posted</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="footer_content">
+          <div class="container">
+            <div class="row footer_content_top">
+              <div class="col-sm-2">
+                <h3>NETWORKS</h3>
+                <p>Projects</p>
+                <p>Contests</p>
+                <p>Sitemap</p>
+                <p>Projects Directory</p>
+                <p>User Directory</p>
+                <p>Freelancer Local</p>
+                <p>Services</p>
+                <p>Escrow</p>
+                <p>Warrior Forum</p>
+                <p>Freenmarket</p>
+              </div>
+              <div class="col-sm-2">
+                <h3>ABOUT</h3>
+                <p>About us </p>
+                <p>How it Works</p>
+                <p>Team</p>
+                <p>Mobile App</p>
+                <p>Desktop App</p>
+                <p>Security</p>
+                <p>Report Bug</p>
+                <p>Fees & Charges</p>
+                <p>Investor</p>
+                <p>Quotes</p>
+              </div>
+              <div class="col-sm-2">
+                <h3>PRESS</h3>
+                <p>In the News</p>
+                <p>Press Releases</p>
+                <p>Awards</p>
+                <p>Testimonials</p>
+                <p>Timeline</p>
+              </div>
+              <div class="col-sm-2">
+                <h3>GET IN TOUCH</h3>
+                <p>Get support</p>
+                <p>Advertise with Us</p>
+                <p>Careers</p>
+                <p>Community</p>
+                <p>Affiliate Program</p>
+                <p>Merchandise</p>
+                <p>Contact Us</p>
+              </div>
+              <div class="col-sm-4 footer_app_store">
+                <img src="assets/images/footer_image_1.png" alt="">
+                <img src="assets/images/footer_image_2.png" alt="">
+                <p>Download the SparkingJobs App</p>
+                <p>Connect with talented freelancers & manage your project wherever you are on the go.</p>
+              </div>
+              <div class="col-sm-12"><div class="footer_line"></div></div>
+              <div class="col-sm-7">
+                <div class="copyright_top">
+                  <span>Privacy Policy</span>
+                  <i>|</i>
+                  <span>Terms and Conditions</span>
+                  <i>|</i>
+                  <span>Copyright Infringement Policy</span>
+                  <i>|</i>
+                  <span>Code of Conduct</span>
+                </div>
+                <div class="copyright_bot">
+                  <p>Sparkingjobs <i class="fa fa-registered" aria-hidden="true"></i> is a registered Trademark of sparking jobs Technology Pty Limited</p>
+                  <p>Sparkingjobs <i class="fa fa-copyright" aria-hidden="true"></i> 2016 sparkingjobs Technology Pty Limited ACN 123456</p>
+                </div>
+              </div>
+              <div class="col-sm-5 footer_link">
+                <i class="fa fa-facebook" aria-hidden="true"></i>
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+                <i class="fa fa-google-plus" aria-hidden="true"></i>
+                <i class="fa fa-linkedin" aria-hidden="true"></i>
+                <i class="fa fa-rss" aria-hidden="true"></i>
+              </div>
+              <div class="col-sm-12 footer_logo">
+                <img src="assets/images/header_logo.png" alt="Sparking Jobs">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script type="text/javascript" src="assets/js/custom.js"></script>
+  <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+
+</body>
+</html>
