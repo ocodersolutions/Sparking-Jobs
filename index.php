8e2cb65345f788312403438ac2b93e0d04bfdfe5
Nguyen Sanh Toan <stoannguyen2205@gmail.com>
2016-11-21 17:17:11 +0700

    update css homepage

--- index.php
+++ index.php
@@ -191,11 +191,8 @@
       background-repeat: repeat;
       color: #777978;
       color: #737373;
-      font-family: Raleway;
     }
-    h1,.h1,h2,.h2,h3,.h3,h4,.h4,h5,.h5,h6,.h6 {
-      font-family: Raleway;
-    }
+    
     #bar-post-place-wrapper {
       background-color: #ffffff !important;
     }
